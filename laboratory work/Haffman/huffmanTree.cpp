ad863b889e6aef43da71fa5505ba29199fd16f9e
Artem Senchenko <moknechnes@mail.ru>
2024-11-18 01:06:07 +0700

    function for inserting nodes to huffman tree has been added

--- laboratory work/Haffman/huffmanTree.cpp
+++ laboratory work/Haffman/huffmanTree.cpp
@@ -1,6 +1,37 @@
 #include "huffmanTree.h"
+#include<iostream>
 
-huffmanTree::huffmanTree()
+node::node()
+{
+
+}
+
+int node::getFrequency()
+{
+	return m_frequency;
+}
+
+void node::setFrequensy(int frequency)
+{
+	m_frequency = frequency;
+}
+
+void node::setBoolVector(boolVector simbols)
+{
+	m_simbols = simbols;
+}
+
+void node::setNext(node* otherNode)
+{
+	next = otherNode;
+}
+
+node* node::getNext()
+{
+	return next;
+}
+
+huffmanTree::huffmanTree(std::string txt)
 {
 
 }
@@ -10,8 +41,59 @@ huffmanTree::~huffmanTree()
 
 }
 
+
+void huffmanTree::insert(std::string simbol, int frequency)
+{
+	if (this->getHead() == nullptr)
+	{
+		node* newNode = new node;
+		boolVector vector(simbol);
+		newNode->setBoolVector(vector);
+		newNode->setFrequensy(frequency);
+		head = newNode;
+		return;
+	}
+	node* newNode = new node;
+	boolVector vector(simbol); // 2 7 5
+	newNode->setBoolVector(vector);
+	newNode->setFrequensy(frequency);
+	if (newNode->getFrequency() < head->getFrequency())
+	{
+		
+		newNode->setNext(head);
+		head = newNode;
+		return;
+	}
+	node* otherNode = head;
+	while (otherNode->getNext()->getFrequency() > newNode->getFrequency())
+	{
+		if (newNode->getFrequency() <= otherNode->getFrequency())
+		{
+			newNode->setNext(otherNode->getNext());
+			otherNode->setNext(newNode);
+			return;
+		}
+		if (otherNode->getNext() == nullptr)
+		{
+			otherNode->setNext(newNode);
+			return;
+		}
+
+		otherNode = otherNode->getNext();
+	}
+
+}
+
 void huffmanTree::build(const std::string& txt)
 {
+	for (int i = 0; i < txt.size(); i++)
+	{
 
+	}
+}
+
+node* huffmanTree::getHead()
+{
+	return head;
 }
 
--- laboratory work/Haffman/huffmanTree.h
+++ laboratory work/Haffman/huffmanTree.h
@@ -2,28 +2,46 @@
 #include"boolVector.h"
 #include<string>
 
+class node
+{
+public:
+
+	node();
+	~node();
+	void setFrequensy(int frequency);
+	void setBoolVector(boolVector vector);
+	void setNext(node* otherNode);
+	
+	node* getNext();
+
+	int getFrequency();
+
+
+
+private:
+	int m_frequency = 1;
+	boolVector m_simbols;
+	node* left = nullptr;
+	node* right = nullptr;
+	node* next = nullptr;
+};
+
 class huffmanTree
 {
 public:
-	class node
-	{
-	public:
+	
 
-		node(boolVector simbols);
-		~node();
-
-	private:
-		int frequency = 1;
-		boolVector m_simbols;
-		node* left = nullptr;
-		node* right = nullptr;
-	};
-
-	huffmanTree();
+	huffmanTree(std::string txt);
 	~huffmanTree();
 
+	node* getHead();
+
 	void build(const std::string &txt);
-
-
+	void insert(std::string simbol, int frequensy);
+private:
+	
+	node* root = nullptr;
+	node* head = nullptr;
+	
 };
 
