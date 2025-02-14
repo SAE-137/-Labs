46c2573295a7fe8a5a510d39891986d2113c922d
Artem Senchenko <moknechnes@mail.ru>
2025-02-14 22:42:10 +0700

    code for function isFileContainsSortedArray has been added

--- laboratory work/FileSort/Source.cpp
+++ laboratory work/FileSort/Source.cpp
@@ -23,11 +23,59 @@ bool createFileWithRandomNumbers(const std::string& fileName, const int numbersC
     return true;
 }
 
-int main()
+void sortFile(const std::string& fileName)
 {
 
+}
 
 
+bool isFileContainsSortedArray(const std::string& fileName)
+{
+    std::ifstream file(fileName);
+    if (!file) return false;
 
-	return 0;
+    int prev, current;
+    if (!(file >> prev)) return false;
+
+    while (file >> current) {
+        if (current < prev) return false;
+        prev = current;
+    }
+    return true;
+}
+
+int createAndSortFile(const std::string& fileName, const int numbersCount, const int maxNumberValue) {
+    if (!createFileWithRandomNumbers(fileName, numbersCount, maxNumberValue)) {
+        return -1;
+    }
+
+    sortFile(fileName); 
+
+    if (!isFileContainsSortedArray(fileName)) {
+        return -2;
+    }
+
+    return 1;
+}
+
+int main() {
+    std::string fileName = "file.txt";
+    const int numbersCount = 1000000;
+    const int maxNumberValue = 100000;
+
+    for (int i = 0; i < 10; i++) {
+        switch (createAndSortFile(fileName, numbersCount, maxNumberValue)) {
+        case 1:
+            std::cout << "Test passed." << std::endl;
+            break;
+        case -1:
+            std::cout << "Test failed: can't create file." << std::endl;
+            break;
+        case -2:
+            std::cout << "Test failed: file isn't sorted." << std::endl;
+            break;
+        }
+    }
+
+    return 0;
 }
