be8818db7faa3c4397e569b02219ac468fbb062e
Artem Senchenko <moknechnes@mail.ru>
2025-02-21 23:39:12 +0700

    insertRec and insert has been added

--- laboratory work/binarySearchTree/binarySearchTree.h
+++ laboratory work/binarySearchTree/binarySearchTree.h
@@ -9,9 +9,13 @@ public:
 
 protected:
 	void insert(int key) override;
+
 	int getMin() const override;
 	int getMax() const override;
 
+	void insert(int key) override;
+
+	node* insertRec(node* node, int key);
 private:
 	node* m_root;
 };
